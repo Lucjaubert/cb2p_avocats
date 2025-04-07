777444eb0feec0487235f108dd72f135cedb70cc
luc_jaubert <j.luc@obvy-app.com>
2025-04-07 22:17:19 +0200

    coorections before deplyoment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost/cb2p-avocats/wp-json/wp/v2',
-  recaptcha: {
-    siteKey: '6Lf1GegqAAAAABOuGDe9DvE4sZFb-8ZDMXhstKXl'
-  }
-};
-
 // export const environment = {
 //   production: true,
-//   apiUrl: 'https://dev.cb2p-avocats.fr/wordpress/wp-json/wp/v2',
+//   apiUrl: 'http://localhost/cb2p-avocats/wp-json/wp/v2',
 //   recaptcha: {
 //     siteKey: '6Lf1GegqAAAAABOuGDe9DvE4sZFb-8ZDMXhstKXl'
 //   }
 // };
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://dev.cb2p-avocats.fr/wordpress/wp-json/wp/v2',
+  recaptcha: {
+    siteKey: '6Lf1GegqAAAAABOuGDe9DvE4sZFb-8ZDMXhstKXl'
+  }
+};
 
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,19 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost/cb2p-avocats/wp-json/wp/v2',
-  recaptcha: {
-    siteKey: '6Lf1GegqAAAAABOuGDe9DvE4sZFb-8ZDMXhstKXl'
-  }
-};
-
-
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://dev.cb2p-avocats.fr/wordpress/wp-json/wp/v2',
+//   apiUrl: 'http://localhost/cb2p-avocats/wp-json/wp/v2',
 //   recaptcha: {
 //     siteKey: '6Lf1GegqAAAAABOuGDe9DvE4sZFb-8ZDMXhstKXl'
 //   }
 // };
+
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://dev.cb2p-avocats.fr/wordpress/wp-json/wp/v2',
+  recaptcha: {
+    siteKey: '6Lf1GegqAAAAABOuGDe9DvE4sZFb-8ZDMXhstKXl'
+  }
+};
 
 
 
